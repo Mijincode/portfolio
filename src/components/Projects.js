e4d862a201e9af8b03908f90bcf4c257cb105ff6
Mijin <mijindoig@hotmail.com>
2024-02-13 10:37:24 +1000

    Refactored code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,65 @@
 import React, { useState } from "react";
 
+const projectData = {
+  HappinessApp: {
+    title: "HappinessApp",
+    videoSrc: "https://www.youtube.com/embed/U-Vrown8XYM",
+    description:
+      "React-based web application to allow users to view and analyse ‘happiness survey’ data drawn from a database.",
+    githubUrl: "https://github.com/Mijincode/HappinessApp",
+  },
+  FlightRoutes: {
+    title: "Flight Routes and Airports",
+    videoSrc: "https://www.youtube.com/embed/n7On5x0mXxo",
+    description:
+      "A team project: Web page that allows a user to interact and perform filtering and analysis with aircraft databases(JSON).",
+    githubUrl: "https://github.com/Mijincode/FlightRoutes",
+  },
+  MovieApp: {
+    title: "Movie API",
+    videoSrc: "https://www.youtube.com/embed/kMa4L73EOr8",
+    description:
+      "Node.js and express-based API, which allows client-side web applications to retrieve data drawn from an SQL database.",
+    githubUrl: "https://github.com/Mijincode/MovieApp",
+  },
+  StyleSerenity: {
+    title: "Style Serenity",
+    videoSrc: "https://www.youtube.com/embed/fN1HkiEuzVA",
+    description:
+      "A fashion web application built using JavaScript, HTML, and CSS which offers a visually appealing and user-friendly interface for browsing and purchasing fashion items.",
+    githubUrl: "https://github.com/Mijincode/StyleSerenity",
+  },
+};
+
 export default function Projects() {
-  const [githubUrls] = useState({
-    HappinessApp: "https://github.com/Mijincode/HappinessApp",
-    FlightRoutes: "https://github.com/Mijincode/FlightRoutes",
-    MovieApp: "https://github.com/Mijincode/MovieApp",
-    StyleSerenity: "https://github.com/Mijincode/StyleSerenity",
-  });
+  const [githubUrls] = useState(projectData);
 
   const handleGithubClick = (project) => {
-    window.open(githubUrls[project], "_blank");
+    window.open(githubUrls[project].githubUrl, "_blank");
   };
+
+  const ProjectCard = ({ project }) => (
+    <div className="card mb-4">
+      <div className="aspect-w-16 aspect-h-15">
+        <iframe
+          title={`${project} Video`}
+          className="w-full h-full"
+          src={projectData[project].videoSrc}
+          allowFullScreen
+        />
+      </div>
+      <div className="card-body">
+        <h4 className="card-title">{projectData[project].title}</h4>
+        <p>{projectData[project].description}</p>
+        <button
+          className="btn btn-primary"
+          onClick={() => handleGithubClick(project)}
+        >
+          View GitHub
+        </button>
+      </div>
+    </div>
+  );
 
   return (
     <>
@@ -25,107 +74,11 @@ export default function Projects() {
       </header>
       <main className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div>
-            <div className="card mb-4">
-              <div className="aspect-w-16 aspect-h-15">
-                <iframe
-                  title="HappinessApp Video"
-                  className="w-full h-full"
-                  src="https://www.youtube.com/embed/U-Vrown8XYM"
-                  allowFullScreen
-                />
-              </div>
-              <div className="card-body">
-                <h4 className="card-title">HappinessApp</h4>
-                <p>
-                  React-based web application to allow users to view and analyse
-                  ‘happiness survey’ data drawn from a database.
-                </p>
-                <button
-                  className="btn btn-primary"
-                  onClick={() => handleGithubClick("HappinessApp")}
-                >
-                  View GitHub
-                </button>
-              </div>
+          {Object.keys(projectData).map((project) => (
+            <div key={project}>
+              <ProjectCard project={project} />
             </div>
-          </div>
-          <div>
-            <div className="card mb-4">
-              <div className="aspect-w-16 aspect-h-15">
-                <iframe
-                  title="Flight Routes and Airports Video"
-                  className="w-full h-full"
-                  src="https://www.youtube.com/embed/n7On5x0mXxo"
-                  allowFullScreen
-                />
-              </div>
-              <div className="card-body">
-                <h4 className="card-title">Flight Routes and Airports</h4>
-                <p>
-                  A team project: Web page that allows a user to interact and
-                  perform filtering and analysis with aircraft databases(JSON).
-                </p>
-                <button
-                  className="btn btn-primary"
-                  onClick={() => handleGithubClick("FlightRoutes")}
-                >
-                  View GitHub
-                </button>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="card mb-4">
-              <div className="aspect-w-16 aspect-h-9">
-                <iframe
-                  title="Movie Video"
-                  className="w-full h-full"
-                  src="https://www.youtube.com/embed/kMa4L73EOr8"
-                  allowFullScreen
-                />
-              </div>
-              <div className="card-body">
-                <h4 className="card-title">Movie API</h4>
-                <p>
-                  Node.js and express-based API, which allows client-side web
-                  applications to retrieve data drawn from an SQL database.
-                </p>
-                <button
-                  className="btn btn-primary"
-                  onClick={() => handleGithubClick("MovieApp")}
-                >
-                  View GitHub
-                </button>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="card mb-4">
-              <div className="aspect-w-16 aspect-h-9">
-                <iframe
-                  title="Style Serenity"
-                  className="w-full h-full"
-                  src="https://www.youtube.com/embed/fN1HkiEuzVA"
-                  allowFullScreen
-                />
-              </div>
-              <div className="card-body">
-                <h4 className="card-title">Style Serenity</h4>
-                <p>
-                  A fashion web application built using JavaScript, HTML, and
-                  CSS which offers a visually appealing and user-friendly
-                  interface for browsing and purchasing fashion items.
-                </p>
-                <button
-                  className="btn btn-primary"
-                  onClick={() => handleGithubClick("StyleSerenity")}
-                >
-                  View GitHub
-                </button>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </main>
     </>
