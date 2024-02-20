a17875501a925ee28ce7f80fa265277ed4b788af
Mijin <mijindoig@hotmail.com>
2024-02-20 11:41:41 +1000

    Changed home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,8 @@ export default function Home() {
               className="rounded-full h-80 w-80 object-cover mb-4"
             />
             <p className="text-lg text-center text-700 p-4 rounded-lg shadow-md">
-              I'm Mijin, a passionate full-stack developer
+              I'm Mijin. Seeking a position where I can contribute my skills to
+              innovative solutions in the field of IT.
             </p>
           </div>
         </div>
