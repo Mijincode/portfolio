25b3907e4113f4a2da98886e1ea003c87c8c8b2c
Mijin <mijindoig@hotmail.com>
2024-07-10 23:02:15 +1000

    Add projectCard

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import { AiFillGithub } from "react-icons/ai";
+
+const ProjectCard = ({ project }) => {
+  const handleGithubClick = (githubUrl) => {
+    window.open(githubUrl, "_blank");
+  };
+
+  return (
+    <Card className="border border-gray-200 rounded-lg">
+      <Card.Body className="d-flex flex-column justify-content-between">
+        <Card.Title className="text-lg font-semibold text-center">
+          {project.title}
+        </Card.Title>
+
+        <div>
+          <iframe
+            title={`${project} Video`}
+            src={project.videoSrc}
+            width="100%"
+            height={"400"}
+          />
+        </div>
+        <Button
+          variant="primary"
+          className="d-flex align-items-center"
+          onClick={() => handleGithubClick(project.githubUrl)}
+        >
+          <AiFillGithub className="w-4 h-4 mr-2" />
+          View GitHub
+        </Button>
+        <Card.Text className="text-gray-700 mt-2">
+          {project.description}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ProjectCard;
