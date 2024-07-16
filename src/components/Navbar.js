dbb63b66020b85968e01ea18c432c6313bd19af6
Mijin <mijindoig@hotmail.com>
2024-07-16 11:08:44 +1000

    Add scrollIntoView

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,26 +18,57 @@ export default function HomeNavbar() {
     };
   }, []);
 
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <header className={` ${scroll ? "sticky" : ""}`}>
       <nav className="container mx-auto">
-        <a href="#home" className="navbar-brand">
+        <a
+          href="#home"
+          className="navbar-brand"
+          onClick={() => scrollToSection("home")}
+        >
           Mijin's Portfolio
         </a>
 
-        <a href="#home" className="nav-link">
+        <a
+          href="#home"
+          className="nav-link"
+          onClick={() => scrollToSection("home")}
+        >
           Home
         </a>
-        <a href="#about" className="nav-link">
+        <a
+          href="#about"
+          className="nav-link"
+          onClick={() => scrollToSection("about")}
+        >
           About
         </a>
-        <a href="#skills" className="nav-link">
+        <a
+          href="#skills"
+          className="nav-link"
+          onClick={() => scrollToSection("skills")}
+        >
           Skills
         </a>
-        <a href="#projects" className="nav-link">
+        <a
+          href="#projects"
+          className="nav-link"
+          onClick={() => scrollToSection("projects")}
+        >
           Projects
         </a>
-        <a href="#contact" className="nav-link">
+        <a
+          href="#contact"
+          className="nav-link"
+          onClick={() => scrollToSection("contact")}
+        >
           Contact
         </a>
       </nav>
