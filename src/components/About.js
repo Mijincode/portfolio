eb7a724aa754a2ca35b9241fe63f8a3ee0157a4a
Mijin <mijindoig@hotmail.com>
2024-11-03 13:52:12 +1000

    Delete card

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../index.css";
-import { Card } from "react-bootstrap";
 import { AcademicCapIcon } from "@heroicons/react/outline";
 
 export default function About() {
