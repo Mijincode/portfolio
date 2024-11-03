6487e6507f385a8d159bd1a86ac1d09eafb69f57
Mijin <mijindoig@hotmail.com>
2024-11-03 13:51:04 +1000

    Delete card

--- src/components/About.js
+++ src/components/About.js
@@ -9,57 +9,55 @@ export default function About() {
       <div className="container px-5 py-10 mx-auto">
         <h2 className="text-4xl font-bold text-center mb-8 mt-10">About Me</h2>
 
-        <div className="max-w-3xl mx-auto">
-          <Card className="mb-4">
-            <Card.Body>
-              <Card.Text className="leading-relaxed text-base ">
-                I have recently completed a Graduate Certificate in Information
-                Technology (Web Development). <br />
-                <br /> Throughout my journey, I've been deeply passionate about
-                both education and technology. My journey into IT began during
-                my high school years, where I developed a strong foundation in
-                technology by mastering software such as Microsoft Word and
-                Excel, and even delving into basic coding. This early exposure
-                sparked my fascination with IT, prompting me to continue
-                learning and exploring new technologies While I initially
-                pursued a career in teaching to make a positive impact in
-                students' lives, my passion for technology remained strong.{" "}
-                <br />
-                <br />
-                As a student teacher, I observed that integrating technology
-                into the classroom had a significant positive impact. This
-                experience motivated me to study more deeply into technologies
-                by experimenting with coding and exploring various software
-                applications outside of the classroom.
-                <br />
-                <br />
-                Now, with a solid understanding of technology fundamentals and
-                valuable skills gained from teaching, I'm eager to transition
-                into a role where I can leverage my coding skills and enthusiasm
-                to further advance my growth and development in the field of
-                software development.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-
-          <Card style={{ marginTop: "50px" }}>
-            <Card.Body>
-              <h2 className="text-4xl font-bold text-center mb-8">
-                Education:
-              </h2>
-              <ul className="leading-relaxed text-base">
-                <li className="flex items-center mb-2">
-                  <AcademicCapIcon className="inline-block w-5 h-5 mr-2 text-green-500" />
-                  Graduate Certificate in Information Technology (Web
-                  Development) - (QUT) | 2023 - 2024
-                </li>
-                <li>
-                  <AcademicCapIcon className="inline-block w-5 h-5 mr-2 text-green-500" />
-                  Master of Teaching (Primary) - (QUT) | 2018 - 2021
-                </li>
-              </ul>
-            </Card.Body>
-          </Card>
+        <div className="max-w-3xl mx-auto text-base leading-relaxed">
+          <p>
+            <strong className="text-xl">From classrooms to code,</strong> my
+            journey has been one driven by curiosity and a deep appreciation for
+            the transformative power of technology. Holding a Graduate
+            Certificate in Information Technology (Web Development), I'm now
+            making an exciting leap from education to the tech industry—a move
+            inspired by the limitless potential technology offers to reshape how
+            we live, work, and learn.
+          </p>
+          <p className="mt-4">
+            It all started in high school, where I first delved into programs
+            like Microsoft Word and Excel, and even experimented with basic
+            coding. These early interactions with technology lit a spark,
+            fueling my fascination with how a few lines of code could accomplish
+            so much. Though I initially chose a career in teaching, aiming to
+            inspire and uplift students, I always carried my passion for tech in
+            my back pocket.
+          </p>
+          <p className="mt-4">
+            As a student teacher, I saw firsthand the incredible impact of
+            technology in the classroom. Simple digital tools could captivate
+            students, turning traditional lessons into interactive experiences
+            that unlocked new levels of engagement and understanding. This
+            revelation inspired me to dig even deeper into coding and tech
+            tools, experimenting beyond the classroom to expand my skills.
+          </p>
+          <p className="mt-4">
+            Now, I'm ready to channel my experience in both education and
+            technology into a software development role, eager to contribute my
+            skills in meaningful ways and explore tech's vast potential. With
+            each new project, I’m driven by the belief that technology holds the
+            power to solve problems, break boundaries, and create a lasting
+            impact—and I’m thrilled to be part of it.
+          </p>
+          <h2 className="text-4xl font-bold text-center my-8 mt-16">
+            Education:{" "}
+          </h2>{" "}
+          <ul className="leading-relaxed text-base">
+            <li className="flex items-center mb-4">
+              <AcademicCapIcon className="inline-block w-5 h-5 mr-2 text-green-500" />
+              Graduate Certificate in Information Technology (Web Development) -
+              (QUT) | 2023 - 2024
+            </li>
+            <li>
+              <AcademicCapIcon className="inline-block w-5 h-5 mr-2 text-green-500" />
+              Master of Teaching (Primary) - (QUT) | 2018 - 2021
+            </li>
+          </ul>
         </div>
       </div>
     </section>
