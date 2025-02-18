c7d51d4c47b216e6dbd2d7cacd9db4c79b372592
Mijin <mijindoig@hotmail.com>
2025-02-18 13:06:28 +1000

    Delete unnecessary

--- src/components/About.js
+++ src/components/About.js
@@ -10,39 +10,6 @@ export default function About() {
 
         <div className="max-w-3xl mx-auto text-base leading-relaxed">
           <p>
-            {/* <strong className="text-xl">From classrooms to code,</strong> my
-            journey has been driven by curiosity and a deep appreciation for the
-            transformative power of technology. Holding a Graduate Certificate
-            in Information Technology (Web Development), I'm now making an
-            exciting leap from education to the tech industry—a move inspired by
-            the limitless potential technology offers to reshape how we live,
-            work, and learn.
-          </p>
-          <p className="mt-4">
-            It all started in high school, where I first delved into programs
-            like Microsoft Word and Excel, and even experimented with basic
-            coding. These early interactions with technology lit a spark,
-            fueling my fascination with how a few lines of code could accomplish
-            so much. Though I initially chose a career in teaching, aiming to
-            inspire and uplift students, I always carried my passion for tech in
-            my back pocket.
-          </p>
-          <p className="mt-4">
-            Throughout my careers in education, I saw firsthand the incredible
-            impact of technology in the classroom. Simple digital tools could
-            captivate students, turning traditional lessons into interactive
-            experiences that unlocked new levels of engagement and
-            understanding. This revelation inspired me to dig even deeper into
-            coding and tech tools, experimenting beyond the classroom to expand
-            my skills.
-          </p>
-          <p className="mt-4">
-            Now, I'm ready to channel my experience in both education and
-            technology into a software development role, eager to contribute my
-            skills in meaningful ways and explore tech's vast potential. With
-            each new project, I’m driven by the belief that technology holds the
-            power to solve problems, break boundaries, and create a lasting
-            impact—and I’m thrilled to be part of it. */}
             <p>
               <strong className="text-xl">From classrooms to code,</strong> my
               journey has been shaped by curiosity, creativity, and a deep
